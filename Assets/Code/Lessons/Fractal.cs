55ae4f5be8984cdf3923d8eac331ded01badd48c
VascoDaKrava <kravchuk.vasilii@yandex.ru>
2022-12-14 17:10:21 +0300

    Jobs Jobs was added.

--- Assets/Code/Lessons/Fractal.cs
+++ Assets/Code/Lessons/Fractal.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 
@@ -13,6 +15,30 @@ namespace SpaceShipRun.Performance
             public Quaternion WorldRotation;
             public float SpinAngle;
         }
+                
+        private struct UpdateFractalLevelJob : IJobFor
+        {
+            public float SpinAngleDelta;
+            public float Scale;
+
+            [ReadOnly]
+            public NativeArray<FractalPart> Parents;
+            public NativeArray<FractalPart> Parts;
+
+            [WriteOnly]
+            public NativeArray<Matrix4x4> Matrices;
+
+            public void Execute(int index)
+            {
+                var parent = Parents[index / _childCount];
+                var part = Parts[index];
+                part.SpinAngle += SpinAngleDelta;
+                part.WorldRotation = parent.WorldRotation * (part.Rotation * Quaternion.Euler(0.0f, part.SpinAngle, 0.0f));
+                part.WorldPosition = parent.WorldPosition + parent.WorldRotation * (_positionOffset * Scale * part.Direction);
+                Parts[index] = part;
+                Matrices[index] = Matrix4x4.TRS(part.WorldPosition, part.WorldRotation, Scale * Vector3.one);
+            }
+        }
 
         [SerializeField] private Mesh _mesh;
         [SerializeField] private Material _material;
@@ -22,8 +48,13 @@ namespace SpaceShipRun.Performance
         private const float _positionOffset = 1.5f;
         private const float _scaleBias = 0.5f;
         private const int _childCount = 5;
-        private FractalPart[][] _parts;
-        private Matrix4x4[][] _matrices;
+
+        //private FractalPart[][] _parts;
+        //private Matrix4x4[][] _matrices;
+
+        private NativeArray<FractalPart>[] _parts;
+        private NativeArray<Matrix4x4>[] _matrices;
+
         private ComputeBuffer[] _matricesBuffers;
         private static readonly int _matricesId = Shader.PropertyToID("_Matrices");
         private static MaterialPropertyBlock _propertyBlock;
@@ -48,16 +79,23 @@ namespace SpaceShipRun.Performance
 
         private void OnEnable()
         {
-            _parts = new FractalPart[_depth][];
-            _matrices = new Matrix4x4[_depth][];
+            //_parts = new FractalPart[_depth][];
+            //_matrices = new Matrix4x4[_depth][];
+            _parts = new NativeArray<FractalPart>[_depth];
+            _matrices = new NativeArray<Matrix4x4>[_depth];
+
             _matricesBuffers = new ComputeBuffer[_depth];
 
             var stride = 16 * 4;
 
             for (int i = 0, length = 1; i < _parts.Length; i++, length *= _childCount)
             {
-                _parts[i] = new FractalPart[length];
-                _matrices[i] = new Matrix4x4[length];
+                //_parts[i] = new FractalPart[length];
+                //_matrices[i] = new Matrix4x4[length];
+
+                _parts[i] = new NativeArray<FractalPart>(length, Allocator.Persistent);
+                _matrices[i] = new NativeArray<Matrix4x4>(length, Allocator.Persistent);
+
                 _matricesBuffers[i] = new ComputeBuffer(length, stride);
             }
 
@@ -84,6 +122,8 @@ namespace SpaceShipRun.Performance
             for (var i = 0; i < _matricesBuffers.Length; i++)
             {
                 _matricesBuffers[i].Release();
+                _parts[i].Dispose();
+                _matrices[i].Dispose();
             }
 
             _parts = null;
@@ -122,29 +162,46 @@ namespace SpaceShipRun.Performance
 
             var scale = 1.0f;
 
+            //for (var li = 1; li < _parts.Length; li++)
+            //{
+            //    scale *= _scaleBias;
+
+            //    var parentParts = _parts[li - 1];
+            //    var levelParts = _parts[li];
+            //    var levelMatrices = _matrices[li];
+
+            //    for (var fpi = 0; fpi < levelParts.Length; fpi++)
+            //    {
+            //        var parent = parentParts[fpi / _childCount];
+            //        var part = levelParts[fpi];
+
+            //        part.SpinAngle += spinAngelDelta;
+            //        deltaRotation = Quaternion.Euler(0.0f, part.SpinAngle, 0.0f);
+
+            //        part.WorldRotation = parent.WorldRotation * part.Rotation * deltaRotation;
+            //        part.WorldPosition = parent.WorldPosition + parent.WorldRotation * (_positionOffset * scale * part.Direction);
+
+            //        levelParts[fpi] = part;
+            //        levelMatrices[fpi] = Matrix4x4.TRS(part.WorldPosition, part.WorldRotation, scale * Vector3.one);
+            //    }
+            //}
+
+            JobHandle jobHandle = default;
+
             for (var li = 1; li < _parts.Length; li++)
             {
                 scale *= _scaleBias;
-
-                var parentParts = _parts[li - 1];
-                var levelParts = _parts[li];
-                var levelMatrices = _matrices[li];
-
-                for (var fpi = 0; fpi < levelParts.Length; fpi++)
+                jobHandle = new UpdateFractalLevelJob
                 {
-                    var parent = parentParts[fpi / _childCount];
-                    var part = levelParts[fpi];
-
-                    part.SpinAngle += spinAngelDelta;
-                    deltaRotation = Quaternion.Euler(0.0f, part.SpinAngle, 0.0f);
-
-                    part.WorldRotation = parent.WorldRotation * part.Rotation * deltaRotation;
-                    part.WorldPosition = parent.WorldPosition + parent.WorldRotation * (_positionOffset * scale * part.Direction);
-
-                    levelParts[fpi] = part;
-                    levelMatrices[fpi] = Matrix4x4.TRS(part.WorldPosition, part.WorldRotation, scale * Vector3.one);
-                }
+                    SpinAngleDelta = spinAngelDelta,
+                    Scale = scale,
+                    Parents = _parts[li - 1],
+                    Parts = _parts[li],
+                    Matrices = _matrices[li]
+                }.Schedule(_parts[li].Length, jobHandle);
             }
+            
+            jobHandle.Complete();
 
             var bounds = new Bounds(rootPart.WorldPosition, 3f * Vector3.one);
 
